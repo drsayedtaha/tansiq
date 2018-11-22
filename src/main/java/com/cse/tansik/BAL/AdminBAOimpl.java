88b1c21b7e3a203ed801565b197b2d4cbcff3034
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2018-11-23 00:32:56 +0200

    add example impelement user login function

---
+++ src/main/java/com/cse/tansik/BAL/AdminBAOimpl.java
@@ -0,0 +1,116 @@
+package com.cse.tansik.BAL;
+
+import com.cse.tansik.DTO.Admin;
+import com.cse.tansik.DTO.Department;
+import com.cse.tansik.DTO.EduYear;
+import com.cse.tansik.DTO.Message;
+import com.cse.tansik.DTO.Prequistes;
+import com.cse.tansik.DTO.Request;
+import com.cse.tansik.DTO.Student;
+import com.cse.tansik.DTO.User;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class AdminBAOimpl implements AdminBAO {
+    public AdminBAOimpl() {
+        super();
+    }
+
+    @Override
+    public Student getData(User user) {
+        // TODO Implement this method
+        return null;
+    }
+
+    @Override
+    public boolean setRequsetsForStudent(List<Request> requests, Student student) {
+        // TODO Implement this method
+        return false;
+    }
+
+    @Override
+    public boolean sendMessage(String text, Student student) {
+        // TODO Implement this method
+        return false;
+    }
+
+    @Override
+    public Message getMessage() {
+        // TODO Implement this method
+        return null;
+    }
+
+    @Override
+    public boolean setDate(Date start, Date end) {
+        // TODO Implement this method
+        return false;
+    }
+
+    @Override
+    public Student getStudent(String userName, String Password) {
+        // TODO Implement this method
+        return null;
+    }
+
+    @Override
+    public boolean editStudent(Student student) {
+        // TODO Implement this method
+        return false;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        // TODO Implement this method
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean editPrequistes(Prequistes preq, Department dep) {
+        // TODO Implement this method
+        return false;
+    }
+
+    @Override
+    public List<Prequistes> getPrequsites(Department dep) {
+        // TODO Implement this method
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Department getDep(EduYear year, String name) {
+        // TODO Implement this method
+        return null;
+    }
+
+    @Override
+    public List<Department> getDeps(EduYear year) {
+        // TODO Implement this method
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Admin getData() {
+        // TODO Implement this method
+        return null;
+    }
+
+    @Override
+    public boolean setData() {
+        // TODO Implement this method
+        return false;
+    }
+
+    @Override
+    public User login(String name, String password) {
+        // TODO Implement this method
+        return null;
+    }
+
+    @Override
+    public boolean addRequest(List<Request> requests) {
+        // TODO Implement this method
+        return false;
+    }
+}
--- src/main/java/com/cse/tansik/BAL/UserBAO.java
+++ src/main/java/com/cse/tansik/BAL/UserBAO.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 public interface UserBAO {
 
-    public User login (String name ,String password);
+     /**
+      * check if username and password is exist in database
+      * and return homepage for user
+      * 
+      * @param userName student or admin user name
+      * @param password 
+      * @return string of home page to be redirected to based in authontication
+      */
+    public String login (String name ,String password);
     public boolean addRequest (List<Request> requests);
 }
