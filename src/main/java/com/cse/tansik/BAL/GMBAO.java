c9b8f2a575e669c600fbeaa2140a02e2fe8740d5
Hussien Mostafa <36410801+HMS-ELKHOLY@users.noreply.github.com>
2018-11-27 00:53:41 +0200

    add DAL object

--- src/main/java/com/cse/tansik/BAL/GMBAO.java
+++ src/main/java/com/cse/tansik/BAL/GMBAO.java
@@ -2,12 +2,13 @@ package com.cse.tansik.BAL;
 import java.util.List;
 
 import com.cse.tansik.DTO.*;
+import com.cse.tansik.DAL.*;
 
 public interface GMBAO extends  AdminBAO    {
   List< Admin>  getAllAdmins();
   List<Student>  getAllStudent();
    boolean editUser(User user);
-   
+   GMDAO gmDAO =new GMDAO();
 
 
 
